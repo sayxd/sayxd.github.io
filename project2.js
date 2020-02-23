5da34ee0cfc139b0a6caafd4ac967c14c1b10b7d
sayxd <42454487+sayxd@users.noreply.github.com>
2020-02-23 15:45:36 -0500

    Add files via upload

--- project2.js
+++ project2.js
@@ -1,5 +1,5 @@
 function spamCheckbox(event){
-    if (event.type == 'click' || event.which ==13){
+    if (event.type == 'click' || event.which ==13 || event.which==32){
       console.log("spam box clicked");
       if($($("#receivespam").children()[0]).attr("src")=="pics/unchecked.png") {
         $($("#receivespam").children()[0]).attr("src","pics/checked.png");
@@ -10,7 +10,9 @@ function spamCheckbox(event){
       }
     }
 
+
 }
+
 
 $(document).ready(function() {
   // Toggle the receive spam checkbox
@@ -29,24 +31,37 @@ $(document).ready(function() {
     }
   });
 
+
   // Form validation
   $("#signupbutton").click(function() {
-    if($("#first_name").val()=="" || $("#middle_initial").val()=="" || $("#last_name").val()=="") {
-      $("#name").addClass("error");
+    if($("#first_name").val()=="") {
+      $("#fn").addClass("error");
+      alert("Please enter value in first name field.")
+      $("#first_name").focus();
 
       return;
     } else {
-      $("#name").removeClass("error");
+      $("#fn").removeClass("error");
+    }
+    if ($("#last_name").val()==""){
+      $("#ln").addClass("error");
+      alert("Please enter value in last name field.")
+      $("#last_name").focus();
+      return;
+    }
+    else {
+      $("#ln").removeClass("error");
     }
 
     if($("#email").val()=="") {
       $("#em").addClass("error");
+      alert("Please enter a value for the Email field.");
+      $("#email").focus();
 
       return;
     } else {
       $("#em").removeClass("error");
     }
-
     alert("Thank you!  Please watch your email for our exciting newsletter and offers!");
   });
 });
